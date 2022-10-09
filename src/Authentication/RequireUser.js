a08574923d63d76edaa4a88bd052245785c2efae
unknown <tobiburrohman2@gmail.com>
2022-10-09 16:59:12 +0600

    first

---
+++ src/Authentication/RequireUser.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Navigate, useLocation, Outlet } from "react-router-dom";
+
+const RequireUser = () => {
+  const [user] = useState(false);
+  const location = useLocation();
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return <Outlet />;
+};
+
+export default RequireUser;
